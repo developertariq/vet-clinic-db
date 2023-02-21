c45541fdd2675c5e81c34c241bcc50129aec6fec
developertariq <dev.tariqul.islam@gmail.com>
2023-02-21 21:18:16 +0600

    Update owner_id field of animals table

--- data.sql
+++ data.sql
@@ -35,3 +35,12 @@ SET species_id = CASE
   ELSE (SELECT id FROM species WHERE name = 'Pokemon')
 END;
 
+UPDATE animals
+SET owner_id = CASE 
+  WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+  WHEN (name = 'Gabumon' OR name = 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+  WHEN (name = 'Devimon' OR name = 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+  WHEN (name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+  WHEN (name = 'Angemon' OR name = 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+END;
+
