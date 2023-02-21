2fa4dddae4c1c90524a60f6c9693e729aa10f461
developertariq <dev.tariqul.islam@gmail.com>
2023-02-21 20:37:18 +0600

    Modify animals table

--- schema.sql
+++ schema.sql
@@ -21,3 +21,11 @@ CREATE TABLE species (
   id SERIAL PRIMARY KEY,
   name TEXT
 );
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INTEGER;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INTEGER;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
